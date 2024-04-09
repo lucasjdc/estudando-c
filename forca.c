51163403684fe1a88a1ad13fadf8bd1adf009b6d
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-04-09 19:08:16 -0300

    Conhecendo os arrays

---
+++ forca.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int notas[10];
+
+    notas[0] = 10;
+    notas[1] = 9;
+    notas[2] = 8;
+    notas[3] = 7;
+
+    printf("notas %d %d %d\n", notas[0], notas[1], notas[2]);
+    
+    return 0;
+}
