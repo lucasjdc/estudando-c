555dc339acfdd8243ca79e9c0501467e2bb534a5
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-04-21 18:23:06 -0300

    Linguagem C Aula 92
    Foi usado `register para sugerir que as variáveis dentro_circulo, x, y e i sejam armazenadas em registradores, se possível.

--- a92.c
+++ a92.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-	register int i;
+#define TOTAL_PONTOS 1000000
+
+double monte_carlo_pi() {
+	register int dentro_circulo = 0;
+	register double x, y;
+	register unsigned int i;
 	
-	for (i = 0; i < 10; i++) {
-		printf("%d\n", i);
+	for (i = 0; i < TOTAL_PONTOS; i++) {
+		x = (double)rand() / RAND_MAX;
+		y = (double)rand() / RAND_MAX;
+		
+		if (x * x + y * y <= 1.0) {
+			dentro_circulo++;
+		}
 	}
 	
-	return 0;
+	return 4.0 * dentro_circulo / TOTAL_PONTOS;
+}
+
+int main() {
+	srand(time(NULL));
 	
+	double pi = monte_carlo_pi();
+	printf("Valor de pi aproximado: %f\n", pi);
+	
+	return 0;
 }
